e2ed7ca3392bac4681555b2e1354b436641245fc
Julie Yu <julie.12.yu@gmail.com>
2024-04-19 23:49:48 -0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css'
 //import {useState} from 'react';
 import MakeRecipe from './components/MakeRecipe';
+import SignIn from './components/SignIn';
 
 function App() {
   //const [name, setName] = useState('');
 
   return (
     <>
+<<<<<<< HEAD
       <MakeRecipe />
+=======
+      <MakeRecipe/>
+      <SignIn />
+>>>>>>> 9d6243bad46946b55e61a073a2fe2eec0b36423c
     </>
   )
   
