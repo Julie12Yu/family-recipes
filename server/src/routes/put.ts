b90b7dc5ba0542d07111dce6a79a79ec9c4b03d3
Julie Yu <julie.12.yu@gmail.com>
2024-04-25 22:17:49 -0700

    Add: Changed post to put to allow for edits in the future (making things more flexible)

---
+++ server/src/routes/put.ts
@@ -0,0 +1,34 @@
+import express, { Router } from 'express';
+export const putRoute = Router();
+import Recipe from '../models/Recipe';
+
+putRoute.put('/put', async (req, res) => {
+    try {
+        const { name, ingredients } = req.body;
+        const item = await Recipe.findOne({ name: name });
+        if (!item) { //POST
+            const newRecipe = new Recipe({
+                name: name,
+                ingredients: ingredients,
+            });
+            await newRecipe.save();
+            res.json(newRecipe);
+        } else { // PUT
+          const editedItem = {
+            name: name,
+            ingredients: ingredients,
+          };
+          const result = await Recipe.findOneAndUpdate({ name: name }, { $set: editedItem }, { new: true });
+          if (result) {
+            res.send(`Recipe with name: ${name} has been updated!`);
+          } else {
+            res.status(404).send(`Recipe with mae: ${name} not found`);
+          }
+        }
+      } catch (err) {
+          console.error('error: ' + err);
+          res.status(400).json({
+            message: "error"
+          }
+      )}
+    });
--- server/src/routes/route.ts
+++ server/src/routes/route.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import {postRoute} from './post';
+import {putRoute} from './put';
 import {deleteRoute} from './delete';
 import {getAllRoute} from './getAll';
 import {getRecipeRoute} from './getRecipe';
 // is middleware? unclear
 export const routes = express.Router();
-routes.use(postRoute);
+routes.use(putRoute);
 routes.use(deleteRoute);
 routes.use(getAllRoute);
 routes.use(getRecipeRoute);
