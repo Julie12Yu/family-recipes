c776ee73be5a96fd54c7c4b99cd2dabc8bff1081
smallboar <williampfang@gmail.com>
2024-04-28 17:53:47 -0700

    Add: POST Login Route

---
+++ server/src/routes/login.ts
@@ -0,0 +1,15 @@
+import express, { Router } from 'express';
+export const loginRoute = Router();
+import Recipe from '../models/Recipe';
+import env from "../../environment.json";
+
+
+//Very bare minimum login system, should be upgraded in future.
+loginRoute.post("/login", async (req, res) => {
+    const {password} = req.body;
+    if (password == env.PASSWORD) {
+        res.send("CORRECT");
+    } else {
+        res.status(401).send("INCORRECT"); //401 is "Unauthorized"
+    }
+});
--- server/src/routes/route.ts
+++ server/src/routes/route.ts
@@ -3,9 +3,12 @@ import {putRoute} from './put';
 import {deleteRoute} from './delete';
 import {getAllRoute} from './getAll';
 import {getRecipeRoute} from './getRecipe';
+import {loginRoute} from './login';
+
 // is middleware? unclear
 export const routes = express.Router();
 routes.use(putRoute);
 routes.use(deleteRoute);
 routes.use(getAllRoute);
 routes.use(getRecipeRoute);
+routes.use(loginRoute);
